317615002cc3d66ab63c1ea0b948f3a6f8f90380
Leroyalle <nikolajmelonov@yandex.ru>
2025-02-03 04:20:37 +0300

    refactor(common): some cleaning

--- utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from 'socket.io';
-
-export default function handleWsError(client: Socket, message: string) {
-  client.emit('error', message);
-  // client.disconnect(true)
-  console.log(message);
-
-  return false;
-}
